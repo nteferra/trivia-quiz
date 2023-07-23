84f38265698f8ced0f8071bdb94c95845e567e66
nteferra <123516477+nteferra@users.noreply.github.com>
2023-07-23 15:31:38 -0400

    set layout for js

--- script.js
+++ script.js
@@ -2,4 +2,66 @@ const container = document.getElementById('container');
 const submitBtn = document.getElementById('submit');
 const results = document.getElementById('results');
 
+const myQuestions = [
+    {
+        question: "",
+        answers: {
 
+        },
+        correctAnswer: "",
+
+    },
+    {
+        question: "",
+        answers: {
+
+        },
+        correctAnswer: "",
+
+    },
+    {
+        question: "",
+        answers: {
+
+        },
+        correctAnswer: "",
+
+    },
+    {
+        question: "",
+        answers: {
+
+        },
+        correctAnswer: "",
+
+    },
+    {
+        question: "",
+        answers: {
+
+        },
+        correctAnswer: "",
+
+    },
+    {
+        question: "",
+        answers: {
+
+        },
+        correctAnswer: "",
+
+    },
+    
+]
+
+
+function buildQuiz() {
+
+}
+
+function showResults() {
+
+}
+
+// build quiz on page load
+buildQuiz()
