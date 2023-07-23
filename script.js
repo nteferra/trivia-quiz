b608135f00b77913372f7efcbfb19fe4409ae9e1
nteferra <123516477+nteferra@users.noreply.github.com>
2023-07-23 16:36:28 -0400

    add some trivia questions

--- script.js
+++ script.js
@@ -2,61 +2,14 @@ const container = document.getElementById('container');
 const submitBtn = document.getElementById('submit');
 const results = document.getElementById('results');
 
-const myQuestions = [
-    {
-        question: "",
-        answers: {
-
-        },
-        correctAnswer: "",
-
-    },
-    {
-        question: "",
-        answers: {
-
-        },
-        correctAnswer: "",
-
-    },
-    {
-        question: "",
-        answers: {
-
-        },
-        correctAnswer: "",
-
-    },
-    {
-        question: "",
-        answers: {
-
-        },
-        correctAnswer: "",
-
-    },
-    {
-        question: "",
-        answers: {
-
-        },
-        correctAnswer: "",
-
-    },
-    {
-        question: "",
-        answers: {
-
-        },
-        correctAnswer: "",
-
-    },
-    
-]
-
-
 function buildQuiz() {
+    //store html output
+    const output = [];
 
+    //for each question
+    myQuestions.forEach(
+
+    )
 }
 
 function showResults() {
@@ -65,3 +18,70 @@ function showResults() {
 
 // build quiz on page load
 buildQuiz()
+
+submitBtn.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+        question: "What is the largest planet in the Galaxy?",
+        answers: {
+            a: "Earth",
+            b: "Jupiter",
+            c: "Uranus",
+        },
+        correctAnswer: "b",
+
+    },
+    {
+        question: "Who proposed the law 'Every action has an equal and opposite reaction'?",
+        answers: {
+            a: "Newton",
+            b: "Einstein",
+            c: "Tesla",
+        },
+        correctAnswer: "a",
+
+    },
+    {
+        question: "Who assassinated Abraham Lincoln in 1865?",
+        answers: {
+            a: "John Wilkes Booth",
+            b: "John Jacob Jones",
+            c: "Lee Harvey Oswald",
+        },
+        correctAnswer: "a",
+
+    },
+    {
+        question: "Who discoverd biological cells?",
+        answers: {
+            a: "Marie Curie",
+            b: "Robert Hooke",
+            c: "Louis Pasteur",
+        },
+        correctAnswer: "b",
+
+    },
+    {
+        question: "Who is known as the father of mathematics?",
+        answers: {
+            a: "Socrates",
+            b: "Pythagoras",
+            c: "Archimedes",
+        },
+        correctAnswer: "c",
+
+    },
+    {
+        question: "Which continent is also a country?",
+        answers: {
+            a: "New Zealand",
+            b: "Antarctica",
+            c: "Australia",
+        },
+        correctAnswer: "c",
+
+    },
+ 
+    
+]
